04250c48c03584c371692792cba00e3ae8bc88b6
theresabartels1410 <theresabartels@gmx.net>
2024-06-22 15:15:13 +0200

    Add files via upload

---
+++ Dashboard.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import TopBar from "../components/TopBar";
+
+export default function Dashboard() {
+    return <>
+        <TopBar/>
+        <h2>Hier kommt noch etwas hin</h2>
+    </>
+}
